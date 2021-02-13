b74789beb293aab8ef6eabb8d083e0f265e3eb7e
Joao Moita Manrique <joaomoitamanrique@gmail.com.br>
2021-02-12 21:51:28 -0300

    Adiciona rotas de produtos

---
+++ src/routes/products.routes.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { getCustomRepository } from "typeorm";
+import Product from "../models/Product";
+import ProductsRepository from "../repositories/ProductsRepository";
+import CreateProductService from "../services/CreateProductService";
+
+const productsRouter = Router();
+
+productsRouter.get("/",  async (request, response) => {
+  const productsRepository = getCustomRepository(ProductsRepository);
+  const products: Product[] = await productsRepository.find();
+
+  return response.json(products);
+});
+
+productsRouter.post("/", async (request, response) => {
+  try {
+    const {
+      name,
+      currentPrice,
+      sku,
+      descrition,
+      shortDescription,
+    } = request.body;
+
+    const createProduct = new CreateProductService();
+
+    const product = await createProduct.execute({
+      name,
+      currentPrice,
+      sku,
+      descrition,
+      shortDescription,
+    });
+
+    return response.json(product);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default productsRouter;
