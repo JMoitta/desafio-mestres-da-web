4310e493119ce0b0d13b5ffa9cf646e182165ddf
Joao Moita Manrique <joaomoitamanrique@gmail.com.br>
2021-02-14 22:30:37 -0300

    Define a rota de 'Variation'

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
+
 import productsRouter from "./products.routes";
 import sessionsRouter from "./sessions.routes";
 import usersRouter from "./users.routes";
+import variatonsRouter from "./variations.routes";
 
 const routes = Router();
 
+routes.use('/variations', variatonsRouter);
 routes.use('/products', productsRouter);
+// routes.use('/product-variations ', sessionsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 
